8ab9c5193e16a31793ccc1cfd410353a1efff6ae
Ponneeshwar <ponneeshwar.p@gmail.com>
2021-06-26 16:37:04 +0530

    2 new spring boot projects
    Added movie-info-service and ratings-data-service projects to the repo

---
+++ ratings-data-service/src/test/java/country/code/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package country.code.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
